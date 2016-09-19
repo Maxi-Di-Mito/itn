a23eccd8c3afcbb21373e61ba417dee7b5b15e15
max90.dm@gmail.com <cele0504>
2016-09-19 18:22:13 -0300

    - try heroku sleep hack

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
  * Created by MaxYCele on 29/4/2016.
  */
 var express = require("express");
+const http = require('http');
 var app = express();
 var serverConfig = require("./server_config");
 var router = require("./router");
@@ -9,9 +10,16 @@ require("babel-polyfill");
 
 app.use(router);
 
+const option = {
+    host: 'www.random.org'
+};
+
 const keepAlive = () =>{
-    setTimeout(keepAlive, 5000);
-    console.log("keepAliveando");
+    setTimeout(keepAlive, 20 * 60 * 1000);
+    console.log("pìngueando");
+    http.request(option,()=>{
+        console.log("Pingueado")
+    });
 };
 
 
