7d2e83138a8e4f613cfc337b2538d0c2e12f8acc
max90.dm@gmail.com <cele0504>
2016-09-20 10:35:49 -0300

    - remove heroku keepAlive

--- server/server.js
+++ server/server.js
@@ -10,28 +10,9 @@ require("babel-polyfill");
 
 app.use(router);
 
-const options = {
-    host: 'https://www.itn-web.herokuapp.com'
-};
-
-const keepAlive = () =>{
-    setTimeout(keepAlive, 20 * 60 * 1000);
-    console.log("pìngueando");
-
-    http.get('http://www.itn-web.herokuapp.com', function(resp){
-        resp.on('data', function(chunk){
-            //do something with chunk
-        });
-    }).on("error", function(e){
-        console.log("Got error: " + e.message);
-    });
-};
-
-
 
 var port = process.env.PORT || 8080;
 
 app.listen(port,()=>{
     console.log("Listening on port "+port);
-    keepAlive();
 });
